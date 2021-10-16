4fe0b47385146f0475d66e83649a1c7c84a60406
Daerckdev <darckflix@gmail.com>
2021-10-16 23:51:44 +0200

    fixed the colors of dmenu with fuzzyhighlight patch

--- config.def.h
+++ config.def.h
@@ -88,7 +88,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, "-shb", selbgcolor, NULL };
 static const char *termcmd[]  = { TERMINAL, NULL };
 static const char *layoutmenu_cmd = "dwm_layoutmenu.sh";
 
