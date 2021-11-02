05153d00756ce8a5a1b04ec1be7bf2a164cd05bd
Daerckdev <darckflix@gmail.com>
2021-11-02 23:00:30 +0100

    insert the values of the gaps in Xresources

--- config.def.h
+++ config.def.h
@@ -8,10 +8,10 @@
 /* appearance */
 static unsigned int borderpx  = 2;        /* border pixel of windows */
 static unsigned int snap      = 10;       /* snap pixel */
-static const unsigned int gappih    = 15;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static unsigned int gappih    = 15;       /* horiz inner gap between windows */
+static unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -105,6 +105,10 @@ ResourcePref resources[] = {
 		{ "borderpx",          	INTEGER, &borderpx },
 		{ "snap",          		INTEGER, &snap },
 		{ "topbar",          	INTEGER, &topbar },
+        { "gappih",             INTEGER, &gappih },
+        { "gappiv",             INTEGER, &gappiv },
+        { "gappoh",             INTEGER, &gappoh },
+        { "gappov",             INTEGER, &gappov },
 };
 
 #include "shiftview.c"
